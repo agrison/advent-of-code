5490b1369f5bc5e1cfe406527d187ba5fcc85112
Alexandre Grison <a.grison@gmail.com>
2020-12-03 08:03:36 +0100

    Day 2

--- src/main/kotlin/days/Day02.kt
+++ src/main/kotlin/days/Day02.kt
@@ -3,18 +3,17 @@ package days
 class Day02 : Day(2) {
     override fun title() = "Password Philosophy"
 
-    override fun partOne() = countValid(inputList) { l, r, letter, pwd ->
+    override fun partOne() = countValid { l, r, letter, pwd ->
         (l..r).contains(pwd.occurrences(letter))
     }
 
-    override fun partTwo() = countValid(inputList) { l, r, letter, pwd ->
+    override fun partTwo() = countValid { l, r, letter, pwd ->
         pwd.at(l - 1).eq(letter) != pwd.at(r - 1).eq(letter)
     }
 
-    private fun countValid(passwords: List<String>,
-                           policy: (a: Int, b: Int, letter: Char, pwd: String) -> Boolean): Int {
+    private fun countValid(policy: (a: Int, b: Int, letter: Char, pwd: String) -> Boolean): Int {
         val regex = Regex("(\\d+)-(\\d+) ([a-z]): ([a-z]+)")
-        return passwords.count {
+        return inputList.count {
             val (l, r, letter, pwd) = regex.find(it)!!.destructured
             policy(l.toInt(), r.toInt(), letter[0], pwd)
         }

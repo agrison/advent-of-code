cc1202d3549e1f20a4b78c54e886c204e4763554
Alexandre Grison <a.grison@gmail.com>
2020-12-03 09:35:58 +0100

    Day 3

--- src/main/kotlin/days/Day03.kt
+++ src/main/kotlin/days/Day03.kt
@@ -9,12 +9,11 @@ class Day03 : Day(3) {
             .map { slope(it / 10, it % 10) }
             .multiply()
 
-    private fun slope(rows: Int, cols: Int): Long {
-        return inputList.mapIndexed { y, row ->
-            val (x, c) = Pair(rows * y, rows * y / cols)
-            (row.debug03(false, c) &&  x.divisible(cols) && row.at(c, '#')).toInt()
-        }.fold(0L, Long::plus)
-    }
+    private fun slope(rows: Int, cols: Int): Long =
+            inputList.mapIndexed { y, row ->
+                val (x, c) = Pair(rows * y, rows * y / cols)
+                (row.debug03(false, c) && x.divisible(cols) && row.at(c, '#')).toInt()
+            }.fold(0L, Long::plus)
 }
 
 

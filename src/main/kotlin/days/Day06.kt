0d4091db267eecc73417cae2024e585594a537c8
Alexandre Grison <a.grison@gmail.com>
2020-12-06 07:05:57 +0100

    Day 6

--- src/main/kotlin/days/Day06.kt
+++ src/main/kotlin/days/Day06.kt
@@ -1,13 +1,15 @@
 package days
 
+import java.lang.System.lineSeparator
+
 class Day06 : Day(6) {
     override fun title() = "Custom Customs"
 
-    override fun partOne() = inputString.split(System.lineSeparator() * 2)
-            .map { it.replace(System.lineSeparator(), "").charSet().size }
+    override fun partOne() = inputString.split(lineSeparator() * 2)
+            .map { it.replace(lineSeparator(), "").charSet().size }
             .sum()
 
-    override fun partTwo() = inputString.split(System.lineSeparator() * 2)
+    override fun partTwo() = inputString.split(lineSeparator() * 2)
             .map { it.split("\n").fold(alphabet.charSet()) { all, s -> all.intersect(s.charSet()) }.size }
             .sum()
 }

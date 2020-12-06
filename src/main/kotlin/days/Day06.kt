7c80936d1a016982dcc3558e81d567b8a3cef00b
Alexandre Grison <a.grison@gmail.com>
2020-12-06 06:58:19 +0100

    Day 6

--- src/main/kotlin/days/Day06.kt
+++ src/main/kotlin/days/Day06.kt
@@ -5,9 +5,9 @@ class Day06 : Day(6) {
 
     override fun partOne() = inputString.split(System.lineSeparator() * 2)
             .map { it.replace(System.lineSeparator(), "").charSet().size }
-            .reduce(Integer::sum)
+            .sum()
 
     override fun partTwo() = inputString.split(System.lineSeparator() * 2)
             .map { it.split("\n").fold(alphabet.charSet()) { all, s -> all.intersect(s.charSet()) }.size }
-            .reduce(Integer::sum)
+            .sum()
 }

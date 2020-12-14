d97b3c4aa9fae2e884e65983684a7eadf3679d43
Alexandre Grison <a.grison@gmail.com>
2020-12-14 09:55:21 +0100

    Day 14

--- src/main/kotlin/days/Day14.kt
+++ src/main/kotlin/days/Day14.kt
@@ -4,11 +4,23 @@ class Day14 : Day(14) {
     override fun title() = "Docking Data"
 
     override fun partOne() = runEmulator { memory, mask, addr, value ->
-        memory[addr] = value or mask.replace("X", "0").binaryLong()
-        memory[addr] = memory[addr]!! and mask.replace("X", "1").binaryLong()
+        memory[addr] = (value or mask.replace("X", "0").binaryLong())
+                .and(mask.replace("X", "1").binaryLong())
     }
 
     override fun partTwo() = runEmulator { memory, mask, addr, value ->
+        fun generateAddresses(addr: Long, mask: String): Sequence<Long> = sequence {
+            if (mask.isNotEmpty()) {
+                generateAddresses(addr / 2, mask.butLast()).forEach { a ->
+                    when (mask.last()) {
+                        'X' -> yieldAll(2 * a .. 2 * a + 1) // floating, may be 0 or 1
+                        '1' -> yield(2 * a + 1)             // overwrite with 1
+                        '0' -> yield(2 * a + addr % 2)      // keep current value
+                    }
+                }
+            } else yield(0L) // stop
+        }
+
         generateAddresses(addr, mask).forEach { memory[it] = value }
     }
 
@@ -16,24 +28,13 @@ class Day14 : Day(14) {
         return mutableMapOf<Long, Long>().let { memory ->
             var mask = ""
             inputList.forEach { l ->
+                val value = l.afterLast(" ")
                 when (l.substring(0, 3)) {
-                    "mas" -> mask = l.afterLast(" ")
-                    "mem" -> version(memory, mask, l.between("[", "]").toLong(), l.afterLast(" ").toLong())
+                    "mas" -> mask = value
+                    "mem" -> version(memory, mask, l.between("[", "]").toLong(), value.toLong())
                 }
             }
             memory.values.sum()
         }
-    }
-
-    private fun generateAddresses(addr: Long, mask: String): Sequence<Long> = sequence {
-        if (mask.isNotEmpty()) {
-            generateAddresses(addr / 2, mask.butLast()).forEach { a ->
-                when (mask.last()) {
-                    'X' -> yieldAll(2 * a..2 * a + 1) // floating, may be 0 or 1
-                    '1' -> yield(2 * a + 1)           // overwrite with 1
-                    '0' -> yield(2 * a + addr % 2)    // keep current value
-                }
-            }
-        } else yield(0L) // stop
     }
 }

91ee663cd557a2bd3cd1dbec7bfa3f30b85d9b4e
Alexandre Grison <a.grison@gmail.com>
2020-12-18 08:03:14 +0100

    Day 18

--- src/main/kotlin/days/Day18.kt
+++ src/main/kotlin/days/Day18.kt
@@ -1,17 +1,51 @@
 package days
 
+import java.util.*
+
 class Day18 : Day(18) {
-    override fun title() = ""
+    override fun title() = "Operation Order"
 
-    override fun partOne(): Int {
+    override fun partOne() = inputList.map { evaluate(parse(it)) }.sum()
 
-        return 0
+    override fun partTwo() = inputList.map { evaluate(parse(it, part2 = true)) }.sum()
+
+    private fun evaluate(tokens: List<String>) = Stack<Long>().let { stack ->
+        tokens.forEach { token ->
+            stack += when (token) {
+                "+" -> stack.pop() + stack.pop()
+                "*" -> stack.pop() * stack.pop()
+                else -> token.toLong()
+            }
+        }
+        stack.last()
     }
 
-    override fun partTwo(): Int {
+    private fun parse(value: String, part2: Boolean = false): List<String> {
+        val expression = mutableListOf<String>()
+        val stack = Stack<String>()
 
-        return 0
+        value.replace(" ", "").map { it.toString() }.forEach { token ->
+            when {
+                token == "(" -> stack += token
+                token.matches("\\d") -> expression += token
+                token in "+*" -> {
+                    while (!stack.isEmpty() &&
+                            (!part2 || (stack.last() != "*" || token != "+"))
+                            && stack.last() != "(")
+                        expression += stack.pop()
+                    stack += token
+                }
+                token == ")" -> {
+                    while (stack.last() != "(")
+                        expression += stack.pop()
+                    stack.popIf("(")
+                }
+            }
+        }
+
+        expression.addAll(stack.reversed())
+        return expression
     }
 }
 
-
+fun <T> Stack<T>.popIf(t: T): T? = if (last() == t) pop() else null

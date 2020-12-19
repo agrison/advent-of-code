78be1e5193db5ccdf1341218777a8ba7a2d3ae9a
Alexandre Grison <a.grison@gmail.com>
2020-12-19 08:25:19 +0100

    Day 19

--- src/main/kotlin/days/Day19.kt
+++ src/main/kotlin/days/Day19.kt
@@ -13,10 +13,12 @@ class Day19 : Day(19) {
     override fun partTwo(): Any {
         val rules = loadRules()
         val resolved = findSolutions(rules)
+        // replacement rules as per part 2 spec
         rules["8"] = "42 | 42 8"
         rules["11"] = "42 31 | 42 11 31"
 
         val solutionsSoFar: MutableSet<String> = mutableSetOf()
+        // iterate until we cannot find a new solution (infinite loop)
         while (true) {
             var foundSolution = false
             iterateRules(rules, resolved)
@@ -34,12 +36,13 @@ class Day19 : Day(19) {
     }
 
     private fun loadRules(): MutableMap<String, String> = mutableMapOf<String, String>().let { rules ->
-        inputString.split(lineSeparator() * 2)[0].split(lineSeparator()).forEach { line ->
+        rules().forEach { line ->
             rules[line.before(":")] = line.after(": ")
         }
         rules
     }
 
+    // iterate on rules until the rule "0" can be fully resolved
     private fun findSolutions(rules: Map<String, String>) = mutableMapOf<String, String>().let { solutions ->
         while (solutionKey !in solutions.keys) {
             iterateRules(rules, solutions)
@@ -47,6 +50,7 @@ class Day19 : Day(19) {
         solutions
     }
 
+    // build a map of regular expressions to match messages with later on
     private fun iterateRules(rules: Map<String, String>, solutions: MutableMap<String, String>) {
         rules.forEach { (number, rule) ->
             when {
@@ -62,5 +66,6 @@ class Day19 : Day(19) {
         }
     }
 
+    private fun rules() = inputString.split(lineSeparator() * 2)[0].split(lineSeparator())
     private fun loadMessages() = inputString.split(lineSeparator() * 2)[1].split(lineSeparator())
 }

9d1dc6211db2f0c05c44a7eeb81382ecc72570d0
Alexandre Grison <a.grison@gmail.com>
2021-01-02 19:54:55 +0100

    2016

--- src/main/kotlin/me/grison/aoc/y2016/Day07.kt
+++ src/main/kotlin/me/grison/aoc/y2016/Day07.kt
@@ -9,18 +9,18 @@ class Day07 : Day(7, 2016) {
     private val supernet = addresses.map { it.filterIndexed { i, _ -> i % 2 == 0 }.joinToString(" ") }
     private val hypernet = addresses.map { it.filterIndexed { i, _ -> i % 2 == 1 }.joinToString(" ") }
 
-    override fun partOne() = supernet.zip(hypernet).count { (sup, hyp) -> is_abba(sup) && !is_abba(hyp) }
+    override fun partOne() = supernet.zip(hypernet).count { (sup, hyp) -> isAbba(sup) && !isAbba(hyp) }
 
-    override fun partTwo() = supernet.zip(hypernet).count { (sup, hyp) -> is_ababab(sup, hyp) }
+    override fun partTwo() = supernet.zip(hypernet).count { (sup, hyp) -> isAbabab(sup, hyp) }
 
-    private fun is_abba(s: String): Boolean {
+    private fun isAbba(s: String): Boolean {
         val (a, b) = p(s, s.substring(1))
         val (c, d) = p(s.substring(2), s.substring(3))
         return d.indices.map { a[it] + b[it] + c[it] + d[it] }
             .any { it[0] + it[1] == it[3] + it[2] && it[0] != it[1] }
     }
 
-    private fun is_ababab(sup: String, hyp: String): Boolean {
+    private fun isAbabab(sup: String, hyp: String): Boolean {
         val (b, c) = p(sup.substring(1), sup.substring(2))
         return c.indices.map { sup[it] + b[it] + c[it] }.any {
             it[0] == it[2] && it[0] != it[1] && ((it[1] + it[0] + it[1]) in hyp)
---
+++ src/main/kotlin/me/grison/aoc/y2016/Day08.kt
@@ -0,0 +1,57 @@
+package me.grison.aoc.y2016
+
+import me.grison.aoc.Day
+import me.grison.aoc.allInts
+import me.grison.aoc.normalSplit
+
+class Day08 : Day(8, 2016) {
+    override fun title() = "Two-Factor Authentication"
+
+    private val width = 50
+    private val height = 6
+
+    private val screen = Array(height) { BooleanArray(width) { false } }
+
+    override fun partOne(): Int {
+        inputList.forEach { line ->
+            val (cmd, arg) = line.normalSplit(" ")
+            val (a, b) = line.allInts()
+            when (cmd) {
+                "rect" -> rect(a, b)
+                "rotate" ->
+                    when (arg) {
+                        "row" -> rotateRow(a, b)
+                        "column" -> rotateColumn(a, b)
+                    }
+            }
+        }
+
+        return screen.fold(0) { acc, it -> acc + it.count { it } }
+    }
+
+    override fun partTwo(): String {
+        return "\n" + screen.joinToString(separator = "\n") { row ->
+            row.joinToString(separator = " ") { col ->
+                if (col) "X" else " "
+            }
+        }
+    }
+
+    fun rect(width: Int, height: Int) {
+        (0 until height).forEach { row ->
+            (0 until width).forEach { col ->
+                screen[row][col] = true
+            }
+        }
+    }
+
+    fun rotateRow(row: Int, by: Int) {
+        (0 until width).map { screen[row][it] }
+            .forEachIndexed { i, b -> screen[row][(i + by) % width] = b }
+    }
+
+    fun rotateColumn(col: Int, by: Int) {
+        (0 until height).map { screen[it][col] }
+            .forEachIndexed { i, b -> screen[(i + by) % height][col] = b }
+    }
+}

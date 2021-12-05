90b6a5d2e77b8bbe1dcad7c8321ade921aae2ee5
Alexandre Grison <a.grison@gmail.com>
2021-12-05 13:54:31 +0100

    Day 05

--- src/main/kotlin/me/grison/aoc/y2021/Day05.kt
+++ src/main/kotlin/me/grison/aoc/y2021/Day05.kt
@@ -9,7 +9,7 @@ import kotlin.collections.component4
 class Day05 : Day(5, 2021) {
     override fun title() = "Hydrothermal Venture"
 
-    override fun partOne() = 0//solve { it[0] == it[2] || it[1] == it[3] }
+    override fun partOne() = solve { it[0] == it[2] || it[1] == it[3] }
 
     override fun partTwo() = solve { true }
 
@@ -25,7 +25,6 @@ class Day05 : Day(5, 2021) {
                     .forEach { (x, y) -> points.increase(p(x, y)) }
             }
 
-        points.filter { it.value > 1 }.forEach { println("[" + it.key.first + ", " + it.key.second + "],") }
         return points.count { it.value > 1 }
     }
 }

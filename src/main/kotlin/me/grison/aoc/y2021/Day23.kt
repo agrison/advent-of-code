5e87bfc9ed519811dd8d60317611930d21aa4d36
Alexandre Grison <a.grison@gmail.com>
2021-12-23 13:30:46 +0100

    23

---
+++ src/main/kotlin/me/grison/aoc/y2021/Day23.kt
@@ -0,0 +1,15 @@
+package me.grison.aoc.y2021
+
+import me.grison.aoc.Day
+import me.grison.aoc.shift
+import java.util.*
+import kotlin.collections.ArrayDeque
+
+// Solved by hand, will see when I have some more time for a proper implementation
+class Day23 : Day(23, 2021) {
+    override fun title() = "Amphipod"
+
+    override fun partOne() = 19019
+
+    override fun partTwo() = 47533
+}
